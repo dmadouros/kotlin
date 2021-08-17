672a496cfd38dc1cb5d2169fdfbeea1e81b35598
David Madouros <david.madouros@gmail.com>
2021-08-16 23:42:26 -0600

    Chapter 9

---
+++ ch09/observe.kts
@@ -0,0 +1,16 @@
+import kotlin.properties.Delegates.observable
+
+var count by observable(0) { property, oldValue, newValue ->
+  println("Property: $property old: $oldValue new: $newValue")
+}
+
+println("The value of count is: $count")
+
+count++
+
+println("The value of count is: $count")
+
+count--
+
+println("The value of count is: $count")
+
---
+++ ch09/veto.kts
@@ -0,0 +1,14 @@
+import kotlin.properties.Delegates.vetoable
+
+var count by vetoable(0) { _, oldValue, newValue -> newValue > oldValue }
+
+println("The value of count is: $count")
+
+count++
+
+println("The value of count is: $count")
+
+count--
+
+println("The value of count is: $count")
+
